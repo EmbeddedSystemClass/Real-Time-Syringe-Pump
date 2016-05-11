453d15931c8b470ee41573d9e5dce4c1c57166b0
Danny Luong <dannyluong408@gmail.com>
2016-05-10 22:27:27 -0700

    Updated

--- main.cpp
+++ main.cpp
@@ -11,13 +11,23 @@
 #include "event_groups.h"
 #include "command_handler.hpp"
 
+//Calculate how many steps per ml of liquid
+#define SYRINGE_VOL_ML 30.0 // How much liquid our syringe holds
+#define SYRINGE_BARREL_LEN_MM 50.0 // Placeholder
+
+#define THREADED_ROD_PITCH 1.25 //
+#define STEPS_PER_REVOLUTION 200.0 //
+#define MICROSTEPS_PER_STEP 16.0 //
+
+long ustepsPerML = (MICROSTEPS_PER_STEP * STEPS_PER_REVOLUTION * SYRINGE_BARREL_LEN_MM) / (SYRINGE_VOL_ML * THREADED_ROD_PITCH );
+//End Calc
 
 QueueHandle_t qh = 0;
 
 class StepperMotor : public scheduler_task
 {
     public:
-    StepperMotor (uint8_t priority) : scheduler_task("watchdog", 2048, priority)
+    StepperMotor (uint8_t priority) : scheduler_task("StepperMotor", 2048, priority)
     {
               /* Nothing to init */
     }
@@ -25,54 +35,62 @@ class StepperMotor : public scheduler_task
     {
         // Set Pins p2.0 - p2.7 To 00 for GPIO Function
         // Can also just do LPC_PINCON->PINSEL4 &= ~FF;
-        LPC_PINCON->PINSEL4 &= ~((1<<0)|(1<<1)); // 2.0 Enable
+        LPC_PINCON->PINSEL4 &= ~((1<<0)|(1<<1)); // 2.0 Dir
         LPC_PINCON->PINSEL4 &= ~((1<<2)|(1<<3)); // 2.1 Step
-        LPC_PINCON->PINSEL4 &= ~((1<<4)|(1<<5)); // 2.2 MS1
-        LPC_PINCON->PINSEL4 &= ~((1<<6)|(1<<7)); // 2.3 MS2
-        LPC_PINCON->PINSEL4 &= ~((1<<8)|(1<<9)); // 2.4 MS3
-        LPC_PINCON->PINSEL4 &= ~((1<<10)|(1<<11)); // 2.5 RESET
-        LPC_PINCON->PINSEL4 &= ~((1<<12)|(1<<13)); // 2.6 DIR
-        LPC_PINCON->PINSEL4 &= ~((1<<14)|(1<<15)); // 2.7 SLEEP
+        LPC_PINCON->PINSEL4 &= ~((1<<4)|(1<<5)); // 2.2 ms1
+        LPC_PINCON->PINSEL4 &= ~((1<<6)|(1<<7)); // 2.3 ms2
+        LPC_PINCON->PINSEL4 &= ~((1<<8)|(1<<9)); // 2.4 ms3
+        LPC_PINCON->PINSEL4 &= ~((1<<10)|(1<<11)); // 2.5 en
 
         /* Set p2.0 - p2.7 to outputs (1) */
         // Can also just do LPC_GPIO2->FIODIR |= FF; Here (same thing) i believe;
         LPC_GPIO2->FIODIR |= (1 << 0);
+        LPC_GPIO2->FIODIR |= (1 << 1);
         LPC_GPIO2->FIODIR |= (1 << 2);
+        LPC_GPIO2->FIODIR |= (1 << 3);
         LPC_GPIO2->FIODIR |= (1 << 4);
-        LPC_GPIO2->FIODIR |= (1 << 6);
-        LPC_GPIO2->FIODIR |= (1 << 8);
-        LPC_GPIO2->FIODIR |= (1 << 10);
-        LPC_GPIO2->FIODIR |= (1 << 12);
-        LPC_GPIO2->FIODIR |= (1 << 14);
+        LPC_GPIO2->FIODIR |= (1 << 5);
+
+        LPC_GPIO2->FIOCLR = (1<<0); // 2.0 Dir starts low goes outward
+        LPC_GPIO2->FIOCLR = (1<<2); // 2.2 ms1 low
+        LPC_GPIO2->FIOCLR = (1<<3); // 2.3 ms2 low
+        LPC_GPIO2->FIOCLR = (1<<4); // 2.4 ms3 low
+        LPC_GPIO2->FIOCLR = (1<<5); // 2.5 enable low - allows motor control
+
+
+
         return true;
     }
     bool run(void *p)
     {
         int steps = 0;
 
-
-        BIT(LPC_GPIO2->FIOPIN).b11_10 = 1;
-
-        //Waits for a message
         if (xQueueReceive(qh,&steps,portMAX_DELAY))
         {
-
             u0_dbg_printf("We have %i steps from the queue receive\n", steps);
 
-            BIT(LPC_GPIO2->FIOPIN).b3_2 = 1;
-            BIT(LPC_GPIO2->FIOPIN).b1_0 = 1;
-            BIT(LPC_GPIO2->FIOPIN).b13_12 = 1;
-
-            for(int b = 0; b < steps;b++)
+            for(int b = 0; b < steps;b++) // Dispense liquid w/ # steps
             {
-                BIT(LPC_GPIO2->FIOPIN).b9_8 = 0;
-                BIT(LPC_GPIO2->FIOPIN).b7_6 = 0;
-                BIT(LPC_GPIO2->FIOPIN).b5_4 = 0;
+            //Motor does 1 step every low to high transition
+            LPC_GPIO2->FIOSET = (1<<1); //2.1 high
+            vTaskDelay(1); //delay
+            LPC_GPIO2->FIOCLR = (1<<1); //2.1 low
+            vTaskDelay(1); //delay
             }
 
+            LPC_GPIO2->FIOSET = (1<<0); // Reverse direction to return to starting position
+            for(int b = 0; b < steps;b++)
+            {
+              //Motor does 1 step every low to high transition
+              LPC_GPIO2->FIOSET = (1<<1); //2.1 high
+              vTaskDelay(1); //delay
+              LPC_GPIO2->FIOCLR = (1<<1); //2.1 low
+              vTaskDelay(1); //delay
+            }
+            LPC_GPIO2->FIOCLR = (1<<0); // Set direction back to outward for next use;
         }
-        BIT(LPC_GPIO2->FIOPIN).b15_14 = 1;
-
+        u0_dbg_printf("Done\n");
+        //vTaskDelay(5000);
 
         /*
         Return to starting position of stepper for next use?
@@ -95,26 +113,24 @@ class BluetoothTask : public scheduler_task
     bool init(void)
     {
         LPC_SC->PCONP |= (1 << 25); // Power Enable UART3
+        LPC_SC->PCLKSEL1 &= -(3 << 18);
 
-        //Enable UART3 PCLK
-        LPC_SC->PCLKSEL1 &= ~(3 << 18); // Clear Values for UART 3 Clock
-        LPC_SC->PCLKSEL1 |= (1 << 18); // Peripheral Clock
+        BIT(LPC_SC->PCLKSEL1).b19_18 = 1; // Peripheral Clock
+        BIT(LPC_UART3->LCR).b7 = 1; // Enable DLAB
 
-        //Enable DLAB
-        LPC_UART3->LCR |= (1 << 7);
-
-        //Sets to 9600 baudrate to match bluetooth
         uint16_t DL = sys_get_cpu_clock()/(9600 * 16);
         LPC_UART3->DLM = DL >> 8;
         LPC_UART3->DLL = DL;
 
-        LPC_PINCON->PINSEL0 &= ~(3 << 0); // Resets Value for pin 0.0
-        LPC_PINCON->PINSEL0 &= ~(3 << 2); // Resets Value for pin 0.1
+        LPC_PINCON->PINSEL0 &= -(3 << 0);
+        LPC_PINCON->PINSEL0 &= -(3 << 2);
 
-        LPC_PINCON->PINSEL0 != (2 << 0); // Sets 0.0 to 10 for TXD3
-        LPC_PINCON->PINSEL0 != (2 << 2); // Sets 0.1 to 10 for RXD3
+        BIT(LPC_PINCON->PINSEL0).b1_0 = 2; // TXD3 P0.0
+        BIT(LPC_PINCON->PINSEL0).b3_2 = 2; // RXD3 P0.1
 
-        LPC_UART3->LCR = 3; //8-bit data and Disabled DLAB
+        LPC_UART3->LCR = 3; //8-bit data
+
+        BIT(LPC_UART3->LCR).b7 = 0; // Disable DLAB
 
         qh = xQueueCreate( 1 , sizeof(int)); // Creates Queue of Size 1
 
@@ -136,37 +152,50 @@ class BluetoothTask : public scheduler_task
 
     bool run(void *p)
     {
+
+        double mlLiquid = 0;
+        char ch = 0;
         int steps;
-        char ch = uart3_getchar(); // Sleeps Till FIFO Not Empty
 
-        //decode message = how many steps on motor
-        switch(ch){
-            case 'a':
-                steps = 1;
-                u0_dbg_printf("%i steps\n", steps);  // Translate data a as 1 step
-                break;
-            case 'b':
-                steps = 10;
-                u0_dbg_printf("%i steps\n", steps); // Translate data b as 10 step
-                break;
-            case 'c':
-                steps = 100;
-                u0_dbg_printf("%i steps\n", steps); // Translate data c as 100 step
-                break;
-            case 'd':
-                steps = 1000;
-                u0_dbg_printf("%i steps\n", steps); // Translate data d as 100 step
-                break;
-            default:
-                //Error
-                break;
+        while (ch != 'z'){ // add liquid until end 'z' signal
+            ch = uart3_getchar(); // grabs byte
+            if (ch == 'a'){ // start signal
+                mlLiquid = 0;
+            }
+            else{
+                switch(ch){
+                         case 'b':
+                             mlLiquid += .05; // .05 mL increment
+                             u0_dbg_printf("Total Liquid: %d\n", mlLiquid);
+                             break;
+                         case 'c':
+                             mlLiquid += .1; // .1 mL increment
+                             u0_dbg_printf("Total Liquid: %d\n", mlLiquid);
+                             break;
+                         case 'd':
+                             mlLiquid += .5; // .5 mL increment
+                             u0_dbg_printf("Total Liquid: %d\n", mlLiquid);
+                             break;
+                         case 'e':
+                             mlLiquid += 1; // 1 mL increment
+                             u0_dbg_printf("Total Liquid: %d\n", mlLiquid);
+                             break;
+                         case 'f':
+                             mlLiquid += 5; // 5 mL increment
+                             u0_dbg_printf("Total Liquid: %d\n", mlLiquid);
+                             break;
+                         case 'g':
+                             mlLiquid += 10; // 10 mL increment
+                             u0_dbg_printf("%i steps\n", steps);
+                             break;
+                         default: break;
+                }
+            }
         }
 
-        //Will basically discard values received from UART3 if not lowercase a,b,c,d
-        if (steps == 1 || steps == 10 || steps == 100 || steps == 1000){
+        steps = mlLiquid * ustepsPerML; //Translate liquid to steps here
+
         xQueueSend(qh, &steps, portMAX_DELAY); // Sends the steps to the stepper task
-        }
-
         return true;
     }
     private:
@@ -176,9 +205,11 @@ class BluetoothTask : public scheduler_task
 
 int main(void)
 {
+    //Relevant Project Tasks
     scheduler_add_task(new terminalTask(PRIORITY_HIGH));
     scheduler_add_task(new StepperMotor(PRIORITY_MEDIUM));
     scheduler_add_task(new BluetoothTask(PRIORITY_LOW));
+
     /* Consumes very little CPU, but need highest priority to handle mesh network ACKs */
 
     /* Change "#if 0" to "#if 1" to run period tasks; @see period_callbacks.cpp */
@@ -190,22 +221,10 @@ int main(void)
     /* The task for the IR receiver */
     // scheduler_add_task(new remoteTask  (PRIORITY_LOW));
 
-    /* Your tasks should probably used PRIORITY_MEDIUM or PRIORITY_LOW because you want the terminal
-     * task to always be responsive so you can poke around in case something goes wrong.
-     */
-
-    /**
-     * This is a the board demonstration task that can be used to test the board.
-     * This also shows you how to send a wireless packets to other boards.
-     */
     #if 0
         scheduler_add_task(new example_io_demo());
     #endif
 
-    /**
-     * Change "#if 0" to "#if 1" to enable examples.
-     * Try these examples one at a time.
-     */
     #if 0
         scheduler_add_task(new example_task());
         scheduler_add_task(new example_alarm());
